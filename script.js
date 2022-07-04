7d5ee9bde5ef67f89bbed8cae7184ee99c1a5909
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-07-04 23:25:44 +0200

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
-let stockPriceElemnt = document.getElementById('stock-price');
+let stockPriceElemnt = document.getElementById('stock-btc');
 let lastPrice = null;
+
+
+let eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade')
+let stockPriceElement2 = document.getElementById('stock-eth')
+
+eth.onmessage = (event)=>{
+    let stockObject = JSON.parse(event.data)
+    let price = parseFloat(stockObject.p).toFixed(2);
+    console.log(price);
+
+    stockPriceElement2.innerText =  price;
+    
+    stockPriceElement2.style.color =!lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red' ; 
+    
+    lastPrice = price; 
+
+}
+
+
+
+
 
 ws.onmessage = (event) => {
 	let stockObject = JSON.parse(event.data);
