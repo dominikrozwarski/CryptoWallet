09a4a82c2d625813a1cb3e2f14b4e9bb2d9f454f
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-07-05 14:23:46 +0200

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+let btc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
 let stockPriceElemnt = document.getElementById('stock-btc');
-let lastPrice = null;
 
 
 let eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade')
 let stockPriceElement2 = document.getElementById('stock-eth')
+
+
+let bnb = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade')
+let stockPriceElement3 = document.getElementById('stock-bnb')
+
+let doge = new WebSocket('wss://stream.binance.com:9443/ws/dogeusdt@trade')
+let stockPriceElement4 = document.getElementById('stock-doge')
+
+let lastPrice = null;
 
 eth.onmessage = (event)=>{
     let stockObject = JSON.parse(event.data)
@@ -19,11 +27,7 @@ eth.onmessage = (event)=>{
 
 }
 
-
-
-
-
-ws.onmessage = (event) => {
+btc.onmessage = (event) => {
 	let stockObject = JSON.parse(event.data);
     let price = parseFloat(stockObject.p).toFixed(2);
     
@@ -33,5 +37,33 @@ ws.onmessage = (event) => {
     
     lastPrice = price; 
 };
+
+bnb.onmessage = (event)=>{
+    let stockObject = JSON.parse(event.data)
+    let price = parseFloat(stockObject.p).toFixed(2);
+    console.log(price);
+
+    stockPriceElement3.innerText =  price;
+    
+    stockPriceElement3.style.color =!lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red' ; 
+    
+    lastPrice = price; 
+
+}
+
+
+doge.onmessage = (event)=>{
+    let stockObject = JSON.parse(event.data)
+    let price = parseFloat(stockObject.p).toFixed(5);
+    console.log(price);
+
+    stockPriceElement4.innerText =  price;
+    
+    stockPriceElement4.style.color =!lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red' ; 
+    
+    lastPrice = price; 
+
+}
+
 
 
