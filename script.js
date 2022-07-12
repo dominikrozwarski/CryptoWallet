792c391148add9a31b6dafa2d2922b1464daec8b
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-07-12 19:32:09 +0200

    first commit

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-// let btc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
-// let stockPriceElemnt = document.getElementById('stock-btc');
+let btc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+let stockPriceElemnt = document.getElementById('stock-btc');
 
 
-// let eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade')
-// let stockPriceElement2 = document.getElementById('stock-eth')
+let eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade')
+let stockPriceElement2 = document.getElementById('stock-eth')
 
 
-// let bnb = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade')
-// let stockPriceElement3 = document.getElementById('stock-bnb')
+let bnb = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade')
+let stockPriceElement3 = document.getElementById('stock-bnb')
 
-// let doge = new WebSocket('wss://stream.binance.com:9443/ws/dogeusdt@trade')
-// let stockPriceElement4 = document.getElementById('stock-doge')
+let doge = new WebSocket('wss://stream.binance.com:9443/ws/dogeusdt@trade')
+let stockPriceElement4 = document.getElementById('stock-doge')
 
-// let lastPrice = null;
+let lastPrice = null;
 
-// eth.onmessage = (event)=>{
-//     let stockObject = JSON.parse(event.data)
-//     let price = parseFloat(stockObject.p).toFixed(2);
-//     console.log(price);
+eth.onmessage = (event)=>{
+    let stockObject = JSON.parse(event.data)
+    let price = parseFloat(stockObject.p).toFixed(2);
+    console.log(price);
 
-//     stockPriceElement2.innerText =  price;
+    stockPriceElement2.innerText =  price;
     
-//     stockPriceElement2.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
+    stockPriceElement2.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
     
-//     lastPrice = price; 
+    lastPrice = price; 
 
-// }
+}
 
-// btc.onmessage = (event) => {
-// 	let stockObject = JSON.parse(event.data);
-//     let price = parseFloat(stockObject.p).toFixed(2);
+btc.onmessage = (event) => {
+	let stockObject = JSON.parse(event.data);
+    let price = parseFloat(stockObject.p).toFixed(2);
     
-// 	stockPriceElemnt.innerText =  price;
+	stockPriceElemnt.innerText =  price;
     
-//     stockPriceElemnt.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
+    stockPriceElemnt.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
     
-//     lastPrice = price; 
-// };
+    lastPrice = price; 
+};
 
-// bnb.onmessage = (event)=>{
-//     let stockObject = JSON.parse(event.data)
-//     let price = parseFloat(stockObject.p).toFixed(2);
-//     console.log(price);
+bnb.onmessage = (event)=>{
+    let stockObject = JSON.parse(event.data)
+    let price = parseFloat(stockObject.p).toFixed(2);
+    console.log(price);
 
-//     stockPriceElement3.innerText =  price;
+    stockPriceElement3.innerText =  price;
     
-//     stockPriceElement3.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
+    stockPriceElement3.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
     
-//     lastPrice = price; 
+    lastPrice = price; 
 
-// }
+}
 
 
-// doge.onmessage = (event)=>{
-//     let stockObject = JSON.parse(event.data)
-//     let price = parseFloat(stockObject.p).toFixed(5);
-//     console.log(price);
+doge.onmessage = (event)=>{
+    let stockObject = JSON.parse(event.data)
+    let price = parseFloat(stockObject.p).toFixed(5);
+    console.log(price);
 
-//     stockPriceElement4.innerText =  price;
+    stockPriceElement4.innerText =  price;
     
-//     stockPriceElement4.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
+    stockPriceElement4.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
     
-//     lastPrice = price; 
+    lastPrice = price; 
 
-// }
+}
 
 
 
