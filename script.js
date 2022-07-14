78bbde5831234fb815911c030a60b64e5d65e8a0
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-07-14 22:21:11 +0200

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // let btc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
 // let stockPriceElemnt = document.getElementById('stock-btc');
 
-
 // let eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade')
 // let stockPriceElement2 = document.getElementById('stock-eth')
-
 
 // let bnb = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade')
 // let stockPriceElement3 = document.getElementById('stock-bnb')
@@ -20,22 +18,22 @@
 //     console.log(price);
 
 //     stockPriceElement2.innerText =  price;
-    
-//     stockPriceElement2.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
-    
-//     lastPrice = price; 
+
+//     stockPriceElement2.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
+
+//     lastPrice = price;
 
 // }
 
 // btc.onmessage = (event) => {
 // 	let stockObject = JSON.parse(event.data);
 //     let price = parseFloat(stockObject.p).toFixed(2);
-    
+
 // 	stockPriceElemnt.innerText =  price;
-    
-//     stockPriceElemnt.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
-    
-//     lastPrice = price; 
+
+//     stockPriceElemnt.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
+
+//     lastPrice = price;
 // };
 
 // bnb.onmessage = (event)=>{
@@ -44,13 +42,12 @@
 //     console.log(price);
 
 //     stockPriceElement3.innerText =  price;
-    
-//     stockPriceElement3.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
-    
-//     lastPrice = price; 
+
+//     stockPriceElement3.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
+
+//     lastPrice = price;
 
 // }
-
 
 // doge.onmessage = (event)=>{
 //     let stockObject = JSON.parse(event.data)
@@ -58,12 +55,30 @@
 //     console.log(price);
 
 //     stockPriceElement4.innerText =  price;
-    
-//     stockPriceElement4.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ; 
-    
-//     lastPrice = price; 
+
+//     stockPriceElement4.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
+
+//     lastPrice = price;
 
 // }
 
-
-
+const fake = document.querySelector('.fake');
+const real = document.querySelector('.real');
+const buy = document.querySelector('.buy');
+const sell = document.querySelector('.sell');
+const accountInfo = document.querySelector('.accountInfo');
+const usdtValue = document.querySelector('.usdtValue');
+const btcValue = document.querySelector('.btcValue');
+const ethValue = document.querySelector('.ethValue');
+const bnbValue = document.querySelector('.bnbValue');
+const dogeValue = document.querySelector('.dogeValue');
+const usdtBtc = document.querySelector('.usdtBtc');
+const usdtEth = document.querySelector('.usdtEth');
+const usdtBnb = document.querySelector('.usdtBnb');
+const usdtDoge = document.querySelector('.usdtDoge');
+const btcUsdt = document.querySelector('.btcUsdt');
+const btcEth = document.querySelector('.btcEth');
+const btcBnb = document.querySelector('.btcBnb');
+const btcDoge = document.querySelector('.btcDoge');
+const walletFake = document.querySelector('.walletFake');
+const close = document.querySelector('.close');
