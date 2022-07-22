2102cd9d2672b3b9982481c72397de7dcd3797d7
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-07-22 15:06:42 +0200

    first commit

--- script.js
+++ script.js
@@ -1,79 +1,91 @@
-// let btc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
-// let stockPriceElemnt = document.getElementById('stock-btc');
+let btc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+let stockPriceElemnt = document.getElementById('stock-btc');
 
-// let eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade')
-// let stockPriceElement2 = document.getElementById('stock-eth')
+let eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade');
+let stockPriceElement2 = document.getElementById('stock-eth');
 
-// let bnb = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade')
-// let stockPriceElement3 = document.getElementById('stock-bnb')
+let bnb = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade');
+let stockPriceElement3 = document.getElementById('stock-bnb');
 
-// let doge = new WebSocket('wss://stream.binance.com:9443/ws/dogeusdt@trade')
-// let stockPriceElement4 = document.getElementById('stock-doge')
+let doge = new WebSocket('wss://stream.binance.com:9443/ws/dogeusdt@trade');
+let stockPriceElement4 = document.getElementById('stock-doge');
 
-// let lastPrice = null;
+let lastPrice = null;
 
-// eth.onmessage = (event)=>{
-//     let stockObject = JSON.parse(event.data)
-//     let price = parseFloat(stockObject.p).toFixed(2);
-//     console.log(price);
+btc.onmessage = (event) => {
+	let stockObject = JSON.parse(event.data);
+	let price = parseFloat(stockObject.p).toFixed(2);
 
-//     stockPriceElement2.innerText =  price;
+	stockPriceElemnt.innerText = price;
 
-//     stockPriceElement2.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
+	stockPriceElemnt.style.color =
+		!lastPrice || lastPrice === price
+			? 'white'
+			: price > lastPrice
+			? 'green'
+			: 'red';
 
-//     lastPrice = price;
+	lastPrice = price;
+};
 
-// }
+eth.onmessage = (event) => {
+	let stockObject = JSON.parse(event.data);
+	let price = parseFloat(stockObject.p).toFixed(2);
 
-// btc.onmessage = (event) => {
-// 	let stockObject = JSON.parse(event.data);
-//     let price = parseFloat(stockObject.p).toFixed(2);
+	stockPriceElement2.innerText = price;
 
-// 	stockPriceElemnt.innerText =  price;
+	stockPriceElement2.style.color =
+		!lastPrice || lastPrice === price
+			? 'white'
+			: price > lastPrice
+			? 'green'
+			: 'red';
 
-//     stockPriceElemnt.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
+	lastPrice = price;
+};
 
-//     lastPrice = price;
-// };
+bnb.onmessage = (event) => {
+	let stockObject = JSON.parse(event.data);
+	let price = parseFloat(stockObject.p).toFixed(2);
 
-// bnb.onmessage = (event)=>{
-//     let stockObject = JSON.parse(event.data)
-//     let price = parseFloat(stockObject.p).toFixed(2);
-//     console.log(price);
+	stockPriceElement3.innerText = price;
 
-//     stockPriceElement3.innerText =  price;
+	stockPriceElement3.style.color =
+		!lastPrice || lastPrice === price
+			? 'white'
+			: price > lastPrice
+			? 'green'
+			: 'red';
 
-//     stockPriceElement3.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
+	lastPrice = price;
+};
 
-//     lastPrice = price;
+doge.onmessage = (event) => {
+	let stockObject = JSON.parse(event.data);
+	let price = parseFloat(stockObject.p).toFixed(5);
 
-// }
+	stockPriceElement4.innerText = price;
 
-// doge.onmessage = (event)=>{
-//     let stockObject = JSON.parse(event.data)
-//     let price = parseFloat(stockObject.p).toFixed(5);
-//     console.log(price);
+	stockPriceElement4.style.color =
+		!lastPrice || lastPrice === price
+			? 'white'
+			: price > lastPrice
+			? 'green'
+			: 'red';
 
-//     stockPriceElement4.innerText =  price;
-
-//     stockPriceElement4.style.color =!lastPrice || lastPrice === price ? 'white' : price > lastPrice ? 'green' : 'red' ;
-
-//     lastPrice = price;
-
-// }
+	lastPrice = price;
+};
 
 const fake = document.querySelector('.fake');
 const real = document.querySelector('.real');
 
-const trade = document.querySelector('.trade');//trade button
-
+const trade = document.querySelector('.trade'); //trade button
 
 const usdtValue = document.querySelector('.usdtValue');
 const btcValue = document.querySelector('.btcValue');
 const ethValue = document.querySelector('.ethValue');
 const bnbValue = document.querySelector('.bnbValue');
 const dogeValue = document.querySelector('.dogeValue');
-
 
 const usdtBtc = document.querySelector('.usdtBtc');
 const usdtEth = document.querySelector('.usdtEth');
@@ -85,39 +97,95 @@ const btcEth = document.querySelector('.btcEth');
 const btcBnb = document.querySelector('.btcBnb');
 const btcDoge = document.querySelector('.btcDoge');
 
+const total = document.querySelector('.total');
+const coinType = document.querySelector('.coinType');
+
+const transBtn = document.querySelector('.transBtn');
+const closeTran = document.querySelector('.closeTran');
+
+const final = document.querySelector('.final');
+
 const walletFake = document.querySelector('.walletFake');
 
 const close = document.querySelector('.close');
 
-const shadow = document.querySelector('.shadow');//shadow while trading
+const shadow = document.querySelector('.shadow'); //shadow while trading
 
-const closeBtn2 = document.querySelector('.closeBtn2');//closing button of trade block
+const closeBtn2 = document.querySelector('.closeBtn2'); //closing button of trade block
 
-const inside = document.querySelector('.inside');//trading block
+const inside = document.querySelector('.inside'); //trading block
 
-const input = document.querySelector('.toSellInput');//trade input
-const tradeBtn = document.querySelector('.tradeBtn');//trade mooney for crytpo btn
-const whatToSell = document.querySelector('.whatToSell');//select coin to sell
-const whatToBuy = document.querySelector('.whatToBuy');//select coin to buy
+const input = document.querySelector('.toSellInput'); //trade input
+const tradeBtn = document.querySelector('.tradeBtn'); //trade mooney for crytpo btn
+const whatToSell = document.querySelector('.SelectOne'); //select coin to sell
+const whatToBuy = document.querySelector('.SelectTwo'); //select coin to buy
 
-const money = document.querySelector('.money');//how mouch you will get of trade
-const coinType = document.querySelector('.coinType');//name of the coin you want to buy in text
+const BtcInside = document.querySelector('#stock-btc'); // btc price
+const EthInside = document.querySelector('#stock-eth'); // eth price
+const BnbInside = document.querySelector('#stock-bnb'); // bnb price
+const DogeInside = document.querySelector('#stock-doge'); //doge price
+
+let inputValue;
+let sell;
+let buy;
+const usdt = 1;
+sell = whatToSell.value;
+buy = whatToBuy.value;
+
+// const getTheValue = (e) => {
+
+// 	inputValue = e.target.value;
+
+// 	console.log(whatToBuy.value);
+// 	console.log(whatToSell.value);
+// 	console.log(DogeInside.textContent);
+
+// };
+
+// console.log(sell);
+
+// tradeBtn.addEventListener('click', getTheValue);
 
 
 
 
-const getTheInput = () =>{
-	console.log(input.value);
-}
 
 
 
 
-addEventListener('keydown', getTheInput())
 
 
 
 
+
+
+
+
+
+
+
+
+const closeFinal = () => {
+	final.style.zIndex = -10;
+	shadow.style.zIndex = 5;
+};
+
+//closing conformation block 
+
+const openFinal = () => {
+	final.style.zIndex = 20;
+	shadow.style.zIndex = 12;
+};
+
+//confirmation of transaction
+
+const finish = () => {
+	final.style.zIndex = -10;
+	shadow.style.zIndex = -10;
+	inside.style.zIndex = -10;
+};
+
+//finishing transaction button
 
 
 
@@ -147,7 +215,9 @@ const closeTrade = () => {
 };
 
 
-
+transBtn.addEventListener('click', finish);
+tradeBtn.addEventListener('click', openFinal);
+closeTran.addEventListener('click', closeFinal);
 
 closeBtn2.addEventListener('click', closeTrade);
 trade.addEventListener('click', openTrade);
